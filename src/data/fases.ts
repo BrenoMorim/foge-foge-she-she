e6687a23e7c63898aab6c7e915994a921a675fc3
BrenoMorim <brenomorim13@gmail.com>
2022-06-04 14:46:07 -0300

    Nova fase labirinto

--- src/data/fases.ts
+++ src/data/fases.ts
@@ -1,148 +1,3 @@
-// export const fases_disponiveis = [
-//     {
-//         id: 1,
-//         label: "Tutorial",
-//         posicaoPersonagens: {
-//             sherlock: {
-//                 i: 1,
-//                 j: 4
-//             },
-//             breno: {
-//                 i: 2,
-//                 j: 7
-//             },
-//             jaminha: {
-//                 i: 2,
-//                 j: 1
-//             }
-//         },
-//         mapa: [
-//             "[-------]".split(""),
-//             "|--}S{--|".split(""),
-//             "|J**E**B|".split(""),
-//             "{-------}".split("")
-//         ] 
-//     },
-//     {
-//         id: 2,
-//         label: "Nível fácil",
-//         posicaoPersonagens: {
-//             sherlock: {
-//                 i: 1,
-//                 j: 4
-//             },
-//             breno: {
-//                 i: 5,
-//                 j: 1
-//             },
-//             jaminha: {
-//                 i: 5,
-//                 j: 7
-//             }
-//         },
-//         mapa: [
-//             "[-------]".split(""),
-//             "|*|*S***|".split(""),
-//             "|****|**|".split(""),
-//             "|*|**|**|".split(""),
-//             "|*{-E{-*|".split(""),
-//             "|B*****J|".split(""),
-//             "{-------}".split("")
-//         ]
-//     },
-//     {
-//         id: 3,
-//         label: "Mais ou menos",
-//         posicaoPersonagens: {
-//             sherlock: {
-//                 i: 1,
-//                 j: 4
-//             },
-//             breno: {
-//                 i: 4,
-//                 j: 4
-//             },
-//             jaminha: {
-//                 i: 1,
-//                 j: 8
-//             }
-//         },
-//         mapa: [
-//             "[--------]".split(""),
-//             "|***S***J|".split(""),
-//             "|*[**|*|*|".split(""),
-//             "|*|**|***|".split(""),
-//             "|*{-B{-**|".split(""),
-//             "|********|".split(""),
-//             "|*{----}*|".split(""),
-//             "|***E****|".split(""),
-//             "{--------}".split("")
-//         ]
-//     },
-//     {
-//         id: 4,
-//         label: "Nível médio",
-//         posicaoPersonagens: {
-//             sherlock: {
-//                 i: 8,
-//                 j: 8
-//             },
-//             breno: {
-//                 i: 8,
-//                 j: 1
-//             },
-//             jaminha: {
-//                 i: 1,
-//                 j: 8
-//             }
-//         },
-//         mapa: [
-//             "[--------]".split(""),
-//             "|E****|*J|".split(""),
-//             "|--*--}**|".split(""),
-//             "|********|".split(""),
-//             "|*|******|".split(""),
-//             "|*{*---]*|".split(""),
-//             "|********|".split(""),
-//             "|*-----}*|".split(""),
-//             "|B******S|".split(""),
-//             "{--------}".split("")
-//         ]
-//     },
-//     {
-//         id: 5,
-//         label: "Nível difícil",
-//         posicaoPersonagens: {
-//             sherlock: {
-//                 i: 1,
-//                 j: 8
-//             },
-//             breno: {
-//                 i: 1,
-//                 j: 1
-//             },
-//             jaminha: {
-//                 i: 6,
-//                 j: 8
-//             }
-//         },
-//         mapa: [
-//             "[--------]".split(""),
-//             "|B******S|".split(""),
-//             "|*[--*-]*|".split(""),
-//             "|*|******|".split(""),
-//             "|*{--**|*|".split(""),
-//             "|******|*|".split(""),
-//             "|*|**|*|J|".split(""),
-//             "|*|*-}*|*|".split(""),
-//             "|******|*|".split(""),
-//             "|*{----}*|".split(""),
-//             "|E*******|".split(""),
-//             "{--------}".split("")
-//         ]
-//     }
-// ]
-
 import IFase from "types/IFase";
 import getPosicaoPersonagens from "util/getPosicaoPersonagens";
 
@@ -151,8 +6,8 @@ const fases_disponiveis = [
         id: 1,
         label: "Tutorial",
         mapa: [
-            "[-------]".split(""),
-            "|--}S{--|".split(""),
+            "   [-]   ".split(""),
+            "[--}S{--]".split(""),
             "|J**E**B|".split(""),
             "{-------}".split("")
         ] 
@@ -271,7 +126,25 @@ const fases_disponiveis = [
             "|J******|".split(""),
             "{-------}".split("")
         ]
-    }
+    },
+    {
+        id: 9,
+        label: "Labirinto",
+        mapa: [
+            "[--------]".split(""),
+            "|J|*****S|".split(""),
+            "|*[--}*]*|".split(""),
+            "|******|*|".split(""),
+            "|*|*[}*|*|".split(""),
+            "|*|*|****|".split(""),
+            "|*|*{*-|*|".split(""),
+            "|*|****|*|".split(""),
+            "|*|*|**|*|".split(""),
+            "|*{---*|*|".split(""),
+            "|E******B|".split(""),
+            "{--------}".split("")
+        ]
+    },
 ] as IFase[];
 
 fases_disponiveis.map(f => {
