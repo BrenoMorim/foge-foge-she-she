6d6fc7dde7abd464ade946db775098711db6a5d1
BrenoMorim <brenomorim13@gmail.com>
2022-08-13 07:30:44 -0300

    Refatorando o menu de pontuação

--- src/components/MenuPontuacao/index.tsx
+++ src/components/MenuPontuacao/index.tsx
@@ -10,12 +10,14 @@ export default function MenuPontuacao({labelFase, pontuacao}: Props) {
     return (
         <section className="pontuacao base__painel-roxo">
             <article className="base__flex-container">
-                <h2 className="pontuacao__titulo">Fase:</h2>
-                <h2 className="pontuacao__subtitulo" data-testid="fase-label">{labelFase}</h2>
+                <h2 className="pontuacao__titulo">
+                    Fase: <span className="pontuacao__subtitulo" data-testid="fase-label">{labelFase}</span>
+                    </h2>
             </article>
             <article className="base__flex-container">
-                <h2 className="pontuacao__titulo">Pontuação:</h2>
-                <h2 className="pontuacao__subtitulo" data-testid="fase-pontuacao">{pontuacao}</h2>
+                <h2 className="pontuacao__titulo">
+                    Pontuação: <span className="pontuacao__subtitulo" data-testid="fase-pontuacao">{pontuacao}</span>
+                </h2>
             </article>
         </section>
     );
